d86f8e1194ffa6fb8dec0da03b395a6ac806ca90
Juan Fernando Marmolejo <juanfer11072007@gmail.com>
2026-07-09 01:35:22 -0500

    liberar procesos, fusionar bloques y fragmentacion

--- allocation/cli.py
+++ allocation/cli.py
@@ -36,6 +36,18 @@ def mostrar_memoria():
     print("\nMemoria total:", MEMORIA_TOTAL, "unidades")
 
 
+def memoria_libre():
+
+    total = 0
+
+    for bloque in memoria:
+
+        if bloque["libre"]:
+            total += bloque["tamano"]
+
+    return total
+
+
 def asignar():
     pid = int(input("ID del proceso: "))
     tam = int(input("Tamaño: "))
@@ -71,6 +83,61 @@ def asignar():
     print("No existe memoria suficiente.")
 
 
+def liberar():
+
+    pid = int(input("Proceso a liberar: "))
+
+    for bloque in memoria:
+
+        if not bloque["libre"] and bloque["pid"] == pid:
+
+            bloque["libre"] = True
+            bloque["pid"] = None
+
+            print("Proceso liberado.")
+            return
+
+    print("Proceso no encontrado.")
+    fusionar()
+
+
+
+def fusionar():
+
+    i = 0
+
+    while i < len(memoria)-1:
+
+        actual = memoria[i]
+        siguiente = memoria[i+1]
+
+        if actual["libre"] and siguiente["libre"]:
+
+            actual["fin"] = siguiente["fin"]
+            actual["tamano"] += siguiente["tamano"]
+
+            memoria.pop(i+1)
+
+        else:
+            i += 1
+
+def fragmentacion_externa():
+
+    libres = []
+
+    for bloque in memoria:
+
+        if bloque["libre"]:
+            libres.append(bloque["tamano"])
+
+    if len(libres) <= 1:
+        return 0
+
+    return sum(libres) - max(libres)
+print()
+
+print("Memoria libre:", memoria_libre())
+print("Fragmentación externa:", fragmentacion_externa()) #Para comprobar
 def main():
 
     while True:
